fc24be5dc88fe94f352ce60f445df661d49f46d2
Artem Shoobovych <shybovycha@gmail.com>
2014-12-07 10:07:22 +0100

    Trying to catch the exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
+
+  rescue_from Exception, :with => :log_error
+
+  def log_error(exception)
+    raise exception unless Rails.env.production?
+
+    now = Time.now
+    timestamp = (now.to_f*1000*1000).to_i
+
+    SULOEXC.error '=================== UNHANDLED EXCEPTION ==================='
+    SULOEXC.error "Message: #{exception.message}"
+
+    SULOEXC.error '========================== TRACE =========================='
+    exception.backtrace.each { |trace| SULOEXC.error trace }
+    SULOEXC.error '=========================== $$$ ==========================='
+
+    render json: { success: false }, status: 202
+  end
 end
